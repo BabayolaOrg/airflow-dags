194fb8ca09699faec6344c58047d9c31cf6ac795
Babayola <148745894+Babayola@users.noreply.github.com>
2024-11-25 17:36:24 +0200

    Updated second_dag.py

--- dags/second_dag.py
+++ dags/second_dag.py
@@ -2,21 +2,10 @@ from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from datetime import datetime
 
-default_args = {
-    'owner': 'airflow',
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
-
 with DAG(
-    'example_dag',
-    default_args=default_args,
-    description='A simple example DAG',
-    schedule_interval=None,
-    start_date=datetime(2023, 11, 1),
-    catchup=False,
+    dag_id="second_dag",
+    schedule_interval="0 0 * * *",
+    start_date=datetime(2023, 11, 25),
+    catchup=False
 ) as dag:
-    start = DummyOperator(task_id='start')
-    end = DummyOperator(task_id='end')
-
-    start >> end
+    start = DummyOperator(task_id="start")
